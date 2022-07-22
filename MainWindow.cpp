54d90db4faf3cad1d32ec301ea2bbc3ebb32f9a9
hariaot <haria.othon@unesp.br>
2022-07-22 09:58:20 -0300

    Atualizando primeira janela

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,2 +1,12 @@
 #include "MainWindow.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	AssemblyCalculator::MainWindow form;
+	Application::Run(% form);
+}
