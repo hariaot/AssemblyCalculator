be2d2ace5cf51abb666040b0f72f861422dff282
hariaot <haria.othon@unesp.br>
2022-07-22 09:46:33 -0300

    Att

---
+++ MainWindow.h
@@ -0,0 +1,69 @@
+#pragma once
+
+namespace AssemblyCalculator {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Sumário para MainWindow
+	/// </summary>
+	public ref class MainWindow : public System::Windows::Forms::Form
+	{
+	public:
+		MainWindow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Adicione o código do construtor aqui
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		~MainWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(680, 409);
+			this->Name = L"MainWindow";
+			this->Text = L"MainWindow";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	}
+	};
+}
